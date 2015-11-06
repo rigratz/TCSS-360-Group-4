b111b2194728f2613250ab59422f09bba03aa667
Riley Gratzer <rigratz@uw.edu>
2015-11-05 17:12:44 -0800

    Started work on menu driven I/O.

--- AuctionCentral/src/AuctionCentral.java
+++ AuctionCentral/src/AuctionCentral.java
@@ -1,10 +1,55 @@
+import java.util.Scanner;
+
 //Testing
 // new branch
 public class AuctionCentral {
 
+	private static Scanner input;
+	private static Calendar auctionCalendar;
+	private static User appUser;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		input = new Scanner(System.in);
+		initializeCalendar();
+		initialMenu();
 
 	}
 
+	public static void initialMenu() {
+		System.out.println("\nWelcome to AuctionCentral!\n--------------------------");
+		System.out.println("\nPlease select your User type\n");
+		System.out.println("1. AuctionCentral Employee\n2. Non-Profit Organization\n3. Bidder");
+		int selection = input.nextInt();
+		switch (selection) {
+			case 1: employeeMenu(); break;
+			case 2: npoMenu(); break;
+			case 3: bidderMenu(); break;
+			default: initialMenu(); break;
+		}
+	}
+	
+	public static void employeeMenu() {
+		//appUser = new Employee();
+		System.out.println("\nHello, Employee! What would you like to do?");
+		System.out.println("\n1. View Calendar\n2. Other Thing");
+		int selection = input.nextInt();
+		switch (selection) {
+			//Will do something.
+			case 1: break;
+			case 2: break;
+			default: employeeMenu(); break;
+		}
+	}
+	public static void npoMenu() {
+		//appUser = new NonProfit();
+		System.out.println("in 2");
+	}
+	public static void bidderMenu() {
+		//appUser = new Bidder();
+		System.out.println("in 3");
+	}
+	
+	private static void initializeCalendar() {
+		auctionCalendar = new Calendar();
+	}
 }
