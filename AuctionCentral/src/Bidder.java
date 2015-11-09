4f8d298793c9e62b623b051b48ba557bd1690b1a
Tempest Parr <parrt@uw.edu>
2015-11-08 22:34:57 -0800

    Abstract user, bidder, AC employee, and non-prof employee added

---
+++ AuctionCentral/src/Bidder.java
@@ -0,0 +1,27 @@
+/**
+ * @author tempest
+ *
+ */
+public class Bidder extends User {
+
+	/**
+	 * * Constructs a Bidder
+	 * @param theName
+	 * @param theContact
+	 */
+	public Bidder(String theName, String theContact) {
+		super(theName, theContact);
+	}
+
+	private void placeBid() {
+		
+	}
+	
+	private void cancelBid() {
+		
+	}
+	
+	private void changeBid() {
+		
+	}
+}
--- AuctionCentral/src/User.java
+++ AuctionCentral/src/User.java
@@ -1,4 +1,57 @@
-
+/**
+ * Abstract User class
+ * @author tempest
+ * 11/8/2015
+ */
 public abstract class User {
 
+	private String myName;		// name of user
+	private String myContact;
+
+	/**
+	 * @param myName
+	 * @param myContact
+	 */
+	public User(String theName, String theContact) {
+		myName = theName;
+		myContact = theContact;
+	}
+
+	// not sure how we are going to display the calendar on console
+	private String viewCalendar(CalendarClass calendar) {
+		calendar.getListOfAuctions();
+	}
+	
+	private void viewAuction(Auction auction) {
+		System.out.println("Auction Name: " + auction.getMyName() +
+				" Auction Date: " + auction.getMyDate());
+	}
+	
+	private void viewItem(Item item) {
+		System.out.println("Item Name: " item.getMyName() + 
+				" Starting Bid: " + item.getMyStartingBid());
+	}
+	
+	// not sure what we want to do here
+	private void logIn() {
+		 
+	}
+
+	public String getMyName() {
+		return myName;
+	}
+
+	public void setMyName(String myName) {
+		this.myName = myName;
+	}
+
+	public String getMyContact() {
+		return myContact;
+	}
+
+	public void setMyContact(String myContact) {
+		this.myContact = myContact;
+	}
+
+	
 }
