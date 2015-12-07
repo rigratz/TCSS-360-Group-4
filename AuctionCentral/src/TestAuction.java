4b044c6183193238575b7f572b7a740015ddd863
William Jensen <williej@uw.edu>
2015-12-07 11:49:27 -0800

    Made finals changes to tests for Item and Auction

--- AuctionCentral/src/TestAuction.java
+++ AuctionCentral/src/TestAuction.java
@@ -13,104 +13,19 @@ public class TestAuction {
 	@Before
 	public void setup() {
 		testAuction = new Auction("testNonProfit", 10, 5, 2016, 5, 6);
-	}
-	
-	@Test
-	public void testAddItem() {
 		Item testLamp = new Item("Lamp", 3.5);
 		testAuction.addItem(testLamp);
+	}
+	
+	@Test
+	public void testAddItemNameIsCorrect() {
 		List<Item> items = testAuction.getMyItems();
 		assertEquals(items.get(0).getMyName(), "Lamp");
-		assertTrue(items.get(0).getMyStartingBid() == 3.5);
 	}
 	
 	@Test
-	public void testSetName() {
-		testAuction.setName("OtherProfit");
-		assertEquals(testAuction.getMyName(), "OtherProfit-10/5/2016");
-	}
-
-	@Test
-	public void testGetName() {
-		assertEquals(testAuction.getMyName(), "testNonProfit-10/5/2016");
-	}
-	
-	@Test
-	public void testSetDate() {
-		testAuction.setMyDate(10, 25, 2016);
-		assertEquals(testAuction.getMyDate(), "10/25/2016");
-		assertEquals(testAuction.getMyName(), "testNonProfit-10/25/2016");
-	}
-	
-	@Test
-	public void testGetDate() {
-		assertEquals(testAuction.getMyDate(), "10/5/2016");
-	}
-	
-	@Test
-	public void testSetItems() {
-		Item testLamp = new Item("Lamp", 3.5);
-		Item testLamp2 = new Item("Lamp2", 4.5);
-		List<Item> items = new ArrayList<Item>();
-		items.add(testLamp);
-		items.add(testLamp2);
-		testAuction.setMyItems(items);
-		List<Item> itemsBack = testAuction.getMyItems();
-		assertEquals(itemsBack.get(0).getMyName(), "Lamp");
-		assertTrue(itemsBack.get(0).getMyStartingBid() == 3.5);
-		testAuction.addItem(testLamp2);
-		items = testAuction.getMyItems();
-		assertEquals(itemsBack.get(1).getMyName(), "Lamp2");
-		assertTrue(itemsBack.get(1).getMyStartingBid() == 4.5);
-	}
-	
-	@Test
-	public void testGetItems() {
-		Item testLamp = new Item("Lamp", 3.5);
-		Item testLamp2 = new Item("Lamp2", 4.5);
-		testAuction.addItem(testLamp);
+	public void testAddItemStartingBidIsCorrect() {
 		List<Item> items = testAuction.getMyItems();
-		assertEquals(items.get(0).getMyName(), "Lamp");
 		assertTrue(items.get(0).getMyStartingBid() == 3.5);
-		testAuction.addItem(testLamp2);
-		items = testAuction.getMyItems();
-		assertEquals(items.get(1).getMyName(), "Lamp2");
-		assertTrue(items.get(1).getMyStartingBid() == 4.5);
-	}
-	
-	@Test
-	public void testGetDay() {
-		assertEquals(testAuction.getMyDay(), 5);
-	}
-	
-	@Test
-	public void testSetDay() {
-		testAuction.setMyDay(6);
-		assertEquals(testAuction.getMyDay(), 6);
-		assertEquals(testAuction.getMyDate(), "10/6/2016");
-	}
-	
-	@Test
-	public void testGetMonth() {
-		assertEquals(testAuction.getMyMonth(), 10);
-	}
-	
-	@Test
-	public void testSetMonth() {
-		testAuction.setMyMonth(6);
-		assertEquals(testAuction.getMyMonth(), 6);
-		assertEquals(testAuction.getMyDate(), "6/5/2016");
-	}
-	
-	@Test
-	public void testGetYear() {
-		assertEquals(testAuction.getMyYear(), 2016);
-	}
-	
-	@Test
-	public void testSetYear() {
-		testAuction.setMyYear(2015);
-		assertEquals(testAuction.getMyYear(), 2015);
-		assertEquals(testAuction.getMyDate(), "10/5/2015");
 	}
 }
--- AuctionCentral/src/TestItem.java
+++ AuctionCentral/src/TestItem.java
@@ -15,10 +15,14 @@ public class TestItem {
 	}
 	
 	@Test
-	public void testBid() {
+	public void testBidLessThanStartingBid() {
 		assertFalse(testItem.bid("Tim", 3.49));
+	}
+	
+	@Test
+	public void testBidEqualToStartingBid() {
+
 		assertTrue(testItem.bid("Jim", 3.5));
-		assertTrue(testItem.bid("Slim", 3.51));
 	}
 	
 	@Test
@@ -31,56 +35,5 @@ public class TestItem {
 		testItem.bid("Tim", 500);
 		testItem.oneBidPerbidder("Tim");
 		assertEquals(testItem.getMyBids().get("Tim"), null);
-	}
-	
-	@Test
-	public void testGetName() {
-		assertEquals(testItem.getMyName(), "testLamp");
-	}
-	
-	@Test
-	public void testSetName() {
-		testItem.setMyName("testChair");
-		assertEquals(testItem.getMyName(), "testChair");
-		System.out.println("Willie likes poop");
-	}
-	
-	@Test
-	public void testGetStartingBid() {
-		double testDouble = 3.5;
-		assertTrue(testItem.getMyStartingBid() == testDouble);
-	}
-	
-	@Test
-	public void testSetStartingBid() {
-		testItem.setMyStartingBid(5.678);
-		double testDouble = 5.678;
-		assertTrue(testItem.getMyStartingBid() == testDouble);
-	}
-
-	@Test
-	public void testGetBids() {
-		assertTrue(testItem.bid("Jim", 3.5));
-		
-		HashMap<String, Double> bids = (HashMap<String, Double>) testItem.getMyBids();
-		assertEquals(bids.get("Jim"), new Double(3.5));
-		assertTrue(testItem.bid("Jim", 4.77));
-		assertEquals(bids.get("Jim"), new Double(4.77));
-	}
-	
-	@Test
-	public void testSetBids() {
-		HashMap<String, Double> testBids = new HashMap<String, Double>();
-		testBids.put("Jim", 3.5);
-		testBids.put("Tim", 3.56);
-		testBids.put("Slim", 13.5);
-		testBids.put("Han Lim", 43.7);
-		testItem.setMyBids(testBids);
-		HashMap<String, Double> bids = (HashMap<String, Double>) testItem.getMyBids();
-		assertEquals(bids.get("Jim"), new Double(3.5));
-		assertEquals(bids.get("Tim"), new Double(3.56));
-		System.out.println("Poop in willies butt");
-		assertEquals(bids.get("Slim"), new Double(13.5));
-		assertEquals(bids.get("Han Lim"), new Double(43.7));
 	}
 }
